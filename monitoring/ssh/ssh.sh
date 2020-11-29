c4c84314b0f29562ce3177b102c04a2fb695bafd
Crexzyz <vsteven25@gmail.com>
2020-11-28 22:17:37 -0600

    Changed subject to differentiate each ssh report

--- monitoring/ssh/ssh.sh
+++ monitoring/ssh/ssh.sh
@@ -45,7 +45,8 @@ function main()
 		journalctl _SYSTEMD_UNIT=sshd.service > $LOGPATH/log_ssh.txt
 		awk -f $ABSPATH/ssh.awk $LOGPATH/log_ssh.txt >> $LOGPATH/ssh_report.log
 		enscript $LOGPATH/ssh_report.log -o - | ps2pdf - $LOGPATH/ssh_report.pdf 
-		cat $LOGPATH/ssh_report.log | mailx -r "virtualcollaboard@gmail.com" -s "SSH REPORT - VIRTUALCOLLABOARD" -a $LOGPATH/ssh_report.pdf $ADDRESS
+		hn=$(hostname)
+		cat $LOGPATH/ssh_report.log | mailx -r "virtualcollaboard@gmail.com" -s "SSH REPORT - $hn" -a $LOGPATH/ssh_report.pdf $ADDRESS
 		printf 'Mail sent to %s\n' $ADDRESS
 	fi
 }
